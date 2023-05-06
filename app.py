9320939b549f8137a6546d923f3218ffe0d22602
Ashley Leal <anleal2004@gmail.com>
2023-05-06 19:04:04 -0400

    Co-authored-by: Zuha1911 <Zuha1911@users.noreply.github.com> Co-authored-by: dan-elle <dan-elle@users.noreply.github.com> Co-authored-by: Weinihsiang <Weinihsiang@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, url_for, request, jsonify
 import cohere
+import cohereIntegration
 
 app = Flask(__name__) 
 
 #app decorator that creates a default route for program; defined in function
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "Hello World"  
 
 if __name__ == "__main__":
     app.run(debug=True)
